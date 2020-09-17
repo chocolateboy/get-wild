3ecd45c5e886b4ff13b5d61ce5970763c252aa0b
chocolateboy <chocolate@cpan.org>
2020-09-17 16:39:20 +0100

    move index.d.ts to the package root

---
+++ index.d.ts
@@ -0,0 +1,32 @@
+declare module 'get-wild/getter' {
+  export type Options = {
+      default?: unknown;
+      flatMap?: PropertyKey | false;
+      map?: PropertyKey | false;
+      parser?: (path: string) => Array<PropertyKey>;
+  };
+  export type Path = string | Array<PropertyKey>;
+  export const getter: (_options?: Options) => {
+      <D, O, T extends unknown>(obj: O, path: Path, $default: D): D | O | T | (D | T)[];
+      <O_1, T_1 extends unknown>(obj: O_1, path: Path): O_1 | T_1 | (T_1 | undefined)[] | undefined;
+  };
+  export const get: {
+      <D, O, T extends unknown>(obj: O, path: Path, $default: D): D | O | T | (D | T)[];
+      <O_1, T_1 extends unknown>(obj: O_1, path: Path): O_1 | T_1 | (T_1 | undefined)[] | undefined;
+  };
+
+}
+declare module 'get-wild/index' {
+  export { get, getter } from 'get-wild/getter';
+  export { default as parser } from 'get-wild/parser';
+
+}
+declare module 'get-wild/parser' {
+  const parser: (path: string) => (string | number)[];
+  export default parser;
+
+}
+declare module 'get-wild' {
+  import main = require('get-wild/index');
+  export = main;
+}
