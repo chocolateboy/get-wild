cff3ac3ab73713400851dd38f80fa208a094df91
chocolateboy <chocolate@cpan.org>
2021-03-05 20:26:42 +0000

    improve parser test output

--- test/parser/invalid.js
+++ test/parser/invalid.js
@@ -1,9 +1,10 @@
 const test      = require('ava')
+const Util      = require('util')
 const { parse } = require('../..')
 const INVALID   = require('./fixtures/_invalid.js')
 
 for (const path of INVALID) {
-    const name = JSON.stringify(path)
+    const name = Util.inspect(path, { compact: true })
 
     test(name, t => {
         t.throws(
--- test/parser/valid.js
+++ test/parser/valid.js
@@ -3,8 +3,10 @@ const Util      = require('util')
 const { parse } = require('../..')
 const VALID     = require('./fixtures/_valid.js')
 
+const dump = value => Util.inspect(value, { compact: true })
+
 for (const [path, want] of Object.entries(VALID)) {
-    const name = Util.format('%j -> %j', path, want)
+    const name = Util.format('%s -> %s', dump(path), dump(want))
 
     test(name, t => {
         const got = parse(path)
